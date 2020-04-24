f80a8c6612b7d7a658237a5b41757111b407d9b6
Shashang <51124608+shashang29@users.noreply.github.com>
2020-04-24 16:38:56 +0545

    minor changes

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
-app.use(express.json());
+const app = express();
+
+app.set('view engine','pug');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname,'public')));
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+
+
 
 app.listen(4000);
