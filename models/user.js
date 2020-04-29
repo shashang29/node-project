45ff9093ff88dd4219deffcbcb45c5e58196b235
Shashang <51124608+shashang29@users.noreply.github.com>
2020-04-29 18:49:02 +0545

    added mongodb

--- models/user.js
+++ models/user.js
@@ -1,16 +1,22 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+ 
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
 
+  }
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users')
+      .findOne({ _id: mongodb.ObjectId(userId)});
+  }
+}
 module.exports = User;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,24 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (cb) => {
+  MongoClient.connect('mongodb+srv://ss:ss@store-fpqdd.gcp.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('connected to mongodb');
+      _db = client.db()
+      cb();
+    })
+    .catch(err => {
+      console.log(err)
+      throw err;
+    });
+}
+const getDb = () => {
+  if (_db) return _db;
+  throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
